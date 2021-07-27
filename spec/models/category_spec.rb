beff576b31f508b3076c0f01c643b07afdab6bad
theIanMilan <theIanMilan@users.noreply.github.com>
2021-07-27 19:23:24 +0800

    Add rspec validation tests to category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valud with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without a description"
 end
