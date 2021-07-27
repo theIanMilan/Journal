05f01b231e4f8efad5fd18de0e49800afd2428b7
theIanMilan <theIanMilan@users.noreply.github.com>
2021-07-27 19:26:12 +0800

    Finish writing validation tests for Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it "is valud with valid attributes"
-  it "is not valid without a title"
-  it "is not valid without a description"
+  it "is valud with valid attributes" do
+    expect(Category.new).to be_valid
+  end
+
+  it "is not valid without a title" do
+    category = Category.new(title: nil)
+    expect(category).to_not be_valid
+  end
+
+  it "is not valid without a description"do
+    category = Category.new(description: nil)
+    expect(category).to_not be_valid
+  end
 end
