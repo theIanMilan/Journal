fdcad2ef1a702ec2330de34b9f1700362271f52e
theIanMilan <theIanMilan@users.noreply.github.com>
2021-07-27 19:40:11 +0800

    Add rpsec test for Editing a category

---
+++ spec/system/editing_journal_categories_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "EditingJournalCategories", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it "edits a category's title and displays it"
+
+  it "edits a category's description and displays it"
+  
+end
