c4cba55501dec7c9b87a90e242fe3c30044282c1
theIanMilan <theIanMilan@users.noreply.github.com>
2021-07-27 21:05:34 +0800

    Add index, show, and edit: controller methods and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
 class CategoriesController < ApplicationController
+  def index
+    @category = Category.order('created_at DESC')
+  end
+  
   def new
     @category = Category.new
   end
@@ -6,6 +10,26 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.create(category_params)
   end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+ end
+
+ def update
+   @category = Category.find(params[:id])
+
+   if @category.update(category_params)
+     flash[:notice] = "category successfully edited!"
+     redirect_to categories_path
+   else
+     flash[:alert] = "Something went wrong..."
+     render :new
+   end
+ end
 
   private
   def category_params
