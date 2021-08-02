a047efcdfe36e336f2115bbf1483d06853bc534c
theIanMilan <theIanMilan@users.noreply.github.com>
2021-08-02 20:22:18 +0800

    Add task model

---
+++ db/migrate/20210802121143_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.integer :category_id
+      t.datetime :deadline, default: -> { 'CURRENT_TIMESTAMP' }
+      t.boolean :completed, default: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_110125) do
+ActiveRecord::Schema.define(version: 2021_08_02_121143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2021_07_27_110125) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "category_id"
+    t.datetime "deadline", default: -> { "CURRENT_TIMESTAMP" }
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
