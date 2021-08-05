e9339601c100290fb46c3c0b485a2c23059701f1
Janson <jansonharold.siy@gmail.com>
2021-08-05 22:10:32 +0800

    updated and edited files for create task feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'categories#index'
 
+  # only for nested
   resources :categories do
     resources :tasks
   end
+
+  get '/tasks', to: 'tasks#index'
 end
