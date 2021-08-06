17fbe02cfd489ac8f9ee7b267f2888f95a6c7f97
Ian Mandap <66746718+theIanMilan@users.noreply.github.com>
2021-08-06 19:36:57 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'categories#index'
 
-  # only for nested
   resources :categories do
     resources :tasks
   end
-
-  get '/tasks', to: 'tasks#index'
 end
