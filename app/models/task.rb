14ef80ed9c347143e73d4b2630f76285d671c811
theIanMilan <theIanMilan@users.noreply.github.com>
2021-08-07 11:49:44 +0800

    Add rspec model test to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :category, dependent: :destroy
+
+  validates_presence_of :title
+  validates_presence_of :description
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @user = User.create(email: 'user@email.com', password: 'password', id: 1)
+    @category = @user.categories.build(title: 'Chores',
+                                       description: 'lorem ipsum',
+                                       user_id: @user.id)
+    @task = @category.tasks.build(title: 'Clean dishes',
+                                  description: 'Clean clean clean',
+                                  category_id: @category.id)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@task).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    @task.title = nil
+    expect(@task).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    @task.description = nil
+    expect(@task).to_not be_valid
+  end
 end
